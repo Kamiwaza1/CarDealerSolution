b2f5bbcb79295a613a64bae852bdaffe76e7614a
Kamiwaza1 <lokzeki@gmail.com>
2025-11-03 15:55:25 +0100

    Add project files.

---
+++ CarDealer.Desktop/LoginForm.cs
@@ -0,0 +1,25 @@
+namespace CarDealer.Desktop
+{
+    public partial class LoginForm : Form
+    {
+        public LoginForm()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void tableLayoutPanel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
